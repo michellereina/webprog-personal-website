f3d6d265b79257bdb33bcd03acc43d40f8878a8e
michellereina <mbpineda@student.apc.edu.ph>
2024-12-18 20:21:36 +0800

    added nickname

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,11 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Michelle";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("M");
+
+  document.getElementById("demo").innerHTML = 'Its ' + element[0].innerHTML;
+
+
+}
