458186e30426de487eb87f9cf0ecf5764792834d
michellereina <mbpineda@student.apc.edu.ph>
2025-01-16 02:37:38 +0800

    .

--- home/js/heartButton.js
+++ home/js/heartButton.js
@@ -1,5 +1,4 @@
-// JavaScript to toggle the "liked" state
 document.querySelector(".heart-container").addEventListener("click", function () {
   const heart = document.querySelector(".heart");
-  heart.classList.toggle("liked"); // Toggle the "liked" class
+  heart.classList.toggle("liked");
 });
