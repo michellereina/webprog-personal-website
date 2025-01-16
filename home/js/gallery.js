bf8ca1c821c2928ed3da07aaa25472ebd3e051ee
michellereina <mbpineda@student.apc.edu.ph>
2025-01-16 15:47:57 +0800

    .

--- home/js/gallery.js
+++ home/js/gallery.js
@@ -2,9 +2,9 @@ const app = Vue.createApp({
     data() {
       return {
         photos: [
-          { name: 'Buying Figurines', url: 'Me buying figurines.jpg' },
-          { name: 'Me in Autumn', url: "Me in Fuji.jpg" },
-          { name: 'Me in Tokyo', url: "Me in Tokyo.jpg" },
+          { name: 'Buying Figurines🐇', url: 'Me buying figurines.jpg' },
+          { name: '📍Kawaguchi', url: "Me in Fuji.jpg" },
+          { name: '📍Tokyo', url: "Me in Tokyo.jpg" },
          
         ]
       };
