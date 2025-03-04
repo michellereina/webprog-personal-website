2acbd592af3e2aac64b5b4702a3b167d09feedc2
michellereina <mbpineda@student.apc.edu.ph>
2025-03-04 22:19:11 +0000

    added int and goals

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,11 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import index from '@/views/index.vue';
 import education from '@/views/education.vue';
+import interest from '@/views/interest.vue';
+import goals from '@/views/goals.vue';
 
 
 const routes = [
     { path: '/', component: index }, // Default route is Login
     { path: '/education', component: education },
+    { path: '/interest', component: interest },
+    { path: '/goals', component: goals },
 ];
 
 const router = createRouter({
