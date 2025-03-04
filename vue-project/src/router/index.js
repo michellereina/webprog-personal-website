8ad7dc9beff3c747390b325cea345834d67e047d
michellereina <mbpineda@student.apc.edu.ph>
2025-03-04 22:15:27 +0000

    added educ

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import index from '@/views/index.vue';
+import education from '@/views/education.vue';
 
 
 const routes = [
     { path: '/', component: index }, // Default route is Login
+    { path: '/education', component: education },
 ];
 
 const router = createRouter({
