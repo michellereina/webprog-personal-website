26f03d74eb78caaa1dd0d4bb92a1a0d11b2e38aa
michellereina <mbpineda@student.apc.edu.ph>
2025-03-04 22:06:41 +0000

    .

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,20 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import education from '@/views/education.vue';
 import index from '@/views/index.vue';
-import interest from '@/views/interest.vue';
-import goals from '@/views/goals.vue';
-import gallery from '@/views/gallery.vue';
-import reference from '@/views/reference.vue';
-import survey from '@/views/survey.vue'
+
 
 const routes = [
     { path: '/', component: index }, // Default route is Login
-    { path: '/education', component: education },
-    { path: '/interest', component: interest },
-    { path: '/goals', component: goals },
-    { path: '/gallery', component: gallery},
-    { path:  '/reference', component: reference},
-    { path:  '/survey', component: survey}
 ];
 
 const router = createRouter({
