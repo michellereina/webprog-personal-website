6f51f2c14d29cca8beb5c2f1d709956cd7f9cc9c
michellereina <mbpineda@student.apc.edu.ph>
2025-03-04 21:49:12 +0000

    removed sparkle

--- vue-project/src/views/index.vue
+++ vue-project/src/views/index.vue
@@ -39,17 +39,6 @@
     </footer>
   </div>
 </template>
-<script>document.addEventListener('mousemove', (e) => {
-    const sparkle = document.createElement('div');
-    sparkle.classList.add('cursor-sparkle');
-    sparkle.style.top = `${e.clientY}px`;
-    sparkle.style.left = `${e.clientX}px`;
-    document.body.appendChild(sparkle);
-  
-    setTimeout(() => {
-      sparkle.remove();
-    }, 500);
-  });
-</script>
+<script></script>
 
 <style></style>
